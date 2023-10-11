32f554809324fcc595a0bd99601a2ae89f17d39c
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2023-10-10 21:54:49 -0300

    Ok

---
+++ src/Fazenda.java
@@ -0,0 +1,9 @@
+public class Fazenda {
+    private static int animais1 = 12;
+    private static Integer animais2 = Integer.valueOf(17);
+
+    public static void main(String args[]){
+        System.out.println("A quantidade de vacas é: " + animais1);
+        System.out.println("A quantidade de carneiros é: " +  animais2);
+    }
+}
