35f66a7091478451783424dde859a6b32b71c08e
Gustavo Cronemberger <gustavocronemberger@gmail.com>
2023-10-25 15:20:11 -0300

    Atv mod10

--- src/br/com/GustavoCronemberger/ControleDeNotas.java
+++ src/br/com/GustavoCronemberger/ControleDeNotas.java
@@ -1,23 +1,33 @@
 package br.com.GustavoCronemberger;
 
+import java.util.Scanner;
+
+import static br.com.GustavoCronemberger.ControleDeNotas.getNota;
+
 public class ControleDeNotas {
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
+        Scanner s = new Scanner(System.in);
 
-        int media1 = 7;
-        int media2 = 6;
-        int media3 = 8;
-        int media4 = 7;
-        int mediaT = (media1 + media2 + media3 + media4)/4;
-        System.out.println("Média Final: " + mediaT);
+        System.out.println("Digite a primeira nota: ");
+        int nota1 = s.nextInt();
+        System.out.println("Digite a segunda nota: ");
+        int nota2 = s.nextInt();
+        System.out.println("Digite a terceira nota: ");
+        int nota3 = s.nextInt();
+        System.out.println("Digite a quarta nota: ");
+        int nota4 = s.nextInt();
+        int nota = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println("Sua média é: " + nota);
+
+        String notaSt = getNota(nota);
+        System.out.println(notaSt);
 
     }
-
-    public static String mediaFinal(int mediaT) {
-
-        if (mediaT >= 7) {
+    public static String getNota (int nota){
+        if (nota >= 7) {
             return "Você está aprovado!";
-        } else if ((mediaT >= 5) && (mediaT <= 6)) {
+        } else if ((nota >= 4) || (nota <= 6)) {
             return "Você está de recuperação!";
         } else {
             return "Você está reprovado!";
